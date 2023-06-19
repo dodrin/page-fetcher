daefb26c28ba1f8c4c5b687a25d5249633d8a061
Rina Inada <indrn@icloud.com>
2023-06-19 00:15:22 +0000

    add json file and .gitignore

--- fetcher.js
+++ fetcher.js
@@ -1,10 +1,3 @@
-// You need to make an http request and wait for the response.
-// After the http request is complete, you need to take the data you receive and write it to a file in your local filesystem.
-
-// It should take two command line arguments:
-// 1. a URL
-// 2. a local file path
-
 const request = require("request");
 const fs = require("fs");
 
@@ -17,6 +10,7 @@ const filePath = args[1];
 request(url, (error, response, body) => {
   console.log("error:", error); // Print the error if one occurred
   console.log("statusCode:", response && response.statusCode);
+  
   //writes a file
   fs.writeFile(filePath, ("body", body), (err) => {
     if (err) {
